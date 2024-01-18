9daf0e6a28f93133892ac59b5bb76d8771c00a2e
황세진 <hwangsejin@hwangsejin-ui-MacBookPro.local>
2024-01-18 20:44:55 +0900

    트리의 지름 코드 수정

--- BOJ 1167 트리의 지름/main.cpp
+++ BOJ 1167 트리의 지름/main.cpp
@@ -9,17 +9,17 @@ int dist[100001];
 void dijkstra(int start) {
     dist[start] = 0;
     std::priority_queue<std::pair<int, int>> pq;
-    pq.push({start, 0});
-    
+    pq.push({0, start});
+
     while (!pq.empty()) {
-        int cur = pq.top().first;
-        int distance = pq.top().second;
+        int cur = pq.top().second;
+        int distance = -pq.top().first;
         pq.pop();
-        
+
         for (auto p : edges[cur]) {
             if (distance + p.second < dist[p.first]) {
                 dist[p.first] = distance + p.second;
-                pq.push({p.first, distance + p.second});
+                pq.push({-(distance + p.second), p.first});
             }
         }
     }
